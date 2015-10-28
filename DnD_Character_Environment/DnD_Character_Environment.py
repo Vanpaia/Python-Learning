48f04f1b7964dd7afca7adfe450e390684b584ed
Hagen <h.schlotzhauer@gmail.com>
2015-10-28 20:14:06 +0100

    Inventory structure added

--- DnD_Character_Environment/DnD_Character_Environment.py
+++ DnD_Character_Environment/DnD_Character_Environment.py
@@ -105,7 +105,34 @@ def new_char_atr():
 		print "Sorry, I don't understand."
 		new_char_atr()
 
-	
+"""
+
+Future structure for inventory
+
+class ShoppingCart(object):
+    """Creates shopping cart objects
+    for users of our fine website."""
+    items_in_cart = {}
+    def __init__(self, customer_name):
+        self.customer_name = customer_name
+
+    def add_item(self, product, price):
+        """Add product to the cart."""
+        if not product in self.items_in_cart:
+            self.items_in_cart[product] = price
+            print product + " added."
+        else:
+            print product + " is already in the cart."
+
+    def remove_item(self, product):
+        """Remove product from the cart."""
+        if product in self.items_in_cart:
+            del self.items_in_cart[product]
+            print product + " removed."
+        else:
+            print product + " is not in the cart."
+
+"""
 		
 
 ##	Strength represents physical power.
