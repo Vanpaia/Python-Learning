cb92e3a6965b91e0b96323eebeca0cec65069f5e
Hagen <h.schlotzhauer@gmail.com>
2015-12-13 18:15:27 +0100

    finally pushing classes experimentation, GUI experimentation adn user creation choice for new_char_atr()

---
+++ DnD_Character_Environment/test_GUI3.py
@@ -0,0 +1,41 @@
+from Tkinter import *
+from PIL import Image, ImageTk
+from DnD_Character_Environment import d
+
+class App:
+	def __init__(self, master):
+		frame = Frame(master)
+		frame.pack()
+		
+		self.button = Button(
+            frame, text="QUIT", fg="red", command=frame.quit
+            )
+		
+		self.button.grid(row=40, column =40, sticky = W)
+		
+		self.b_dice = Button(frame, text="Throw d20", command=self.dice)
+		self.b_dice.grid(row=0, column = 0, columnspan = 4, sticky = W)
+		
+		self.result = ""
+		self.label_text = StringVar()
+		self.label_text.set(self.result)
+		self.label = Label(frame, textvariable = self.label_text)
+		self.label.grid(row=0, column = 5, sticky = W)
+		
+	def dice(self):
+		self.result = d(20)
+		self.label_text.set(self.result)
+
+root = Tk()
+root.resizable(width=False, height=False)
+root.geometry("800x600")
+
+import_background = Image.open('C:\\Users\Hagen\Desktop\DnD_Artwork\App_Background.png')
+image_background = ImageTk.PhotoImage(import_background)
+background = Label(root, image = image_background)
+background.place(x=0, y=0, relwidth=1, relheight=1)
+
+app = App(root)
+
+root.mainloop()
+root.destroy()
--- DnD_Character_Environment/test_choice_atr.py
+++ DnD_Character_Environment/test_choice_atr.py
@@ -1,40 +1,3 @@
-##############################################
-##############################################
-##											##
-##			Dungeons & Dragons				##
-##		Player's Character Environment		##
-##					v0.1					##
-##											##
-##############################################
-##############################################
-
-
-## Plans for v0.1
-## 
-##	- Character creator						x
-## 		- Random							x
-##		- Custom							x
-##
-##	- Combat
-##		- Turn order
-##	- Inventory
-##	- Equipment
-##	- Encounters
-##	- Leveling
-##		- Paragon Path at level 11
-##		- Epic Destiny
-##	- Keep track of AP
-##
-##	- Sleeping
-##		- Refill health/ap
-##
-##	- Saving								x
-##	- Loading								x
-
-from random import randint
-import pickle
-
-gamestate = "continue"
 
 class Characters(object):
 	
@@ -42,7 +5,7 @@ class Characters(object):
 		self.exp = exp
 		self.gender = gender
 		self.name = name
-		self.race = Dragonborn()
+		self.race = race
 		self.age = age
 		self.height = height
 		self.weight = weight
@@ -120,99 +83,178 @@ class Characters(object):
 			return 30
 
 
-class Dragonborn(object):
-	def __init__(self):
-		self.name = "Dragonborn"
-		self.max_h = 203
-		self.min_h = 188
-		self.max_w = 145
-		self.min_w = 100
-	def ability_scores(self):
-		char.str = char.str +2
-		char.char = char.char +2
-	def test(self):
-		return "succes"
-			
-races = ['deva', 'dragonborn', 'drow', 'dwarf', 'eladrin', 'elf', 'genasi', 'githzerai', 'gnoll', 'gnome', 'goblin', 'goliath', 'half-elf', 'half-orc', 'halfling', 'hamadryad', 'human', 'kalashtar', 'shifter', 'minotaur', 'mul', 'pixie', 'revenant', 'satyr', 'shadar-kai', 'shade', 'shardmind', 'thri-kreen', 'tiefling', 'vryloka', 'warforged', 'wilden']
-classes = ['ardent', 'avenger', 'barbarian', 'bard', 'battlemind', 'cleric', 'druid', 'fighter', 'invoker', 'monk', 'paladin', 'psion', 'ranger', 'rogue', 'runepriest', 'seeker', 'shaman', 'sorcerer', 'warden', 'warlock', 'warlord', 'wizard']
+char = Characters(0, "Male", "Hagen", "Elf", 19, 192, 90, "Cleric", "Good", "Allah")
+attributes = ["str", "con", "dex", "int", "wis", "char"]
+attribute_cost = {10:0, 11:1, 12:2, 13:3, 14:5, 15:7, 16:9, 17:12, 18:16}
+attribute_cost_weakness = {8:0, 9:1, 10:2, 11:3, 12:4, 13:5, 14:7, 15:9, 16:11, 17:14, 18:18}
 
-def help():
-	print "Welcome to the D&D Player\'s Character Environment v0.1"
-	print "Now go dungeoneering!"
-
-def save():
-	file = open("save", "w")
-	pickle.dump(char, file)
-	file.close
-
-def load():
-	global char
-	load_file = open("save", "r")
-	char = pickle.load(load_file)
-	load_file.close()
-
-def exit():
-	global gamestate
-	gamestate = "quit"
-	
-def d(sides, throws = 1, advantage = 0):
-	result = []
-	if advantage == 0:	
-		for i in range(throws):
-			result.append(randint(1, sides))
+def atr_by_choice():
+	points_left = 22
+	print ""
+	weakpoint = str(raw_input("What attribute is your weakpoint? \n(str, con, dex, int, wis, char) "))
+	while weakpoint not in attributes:
+		print "not a valid attribute"
+		weakpoint = raw_input("What attribute is your weakpoint? \n(str, con, dex, int, wis, char) ")
+	if weakpoint.lower() == "str":
+		char.str = 8
+	elif weakpoint.lower() == "con":
+		char.con = 8
+	elif weakpoint.lower() == "dex":
+		char.dex = 8
+	elif weakpoint.lower() == "int":
+		char.int = 8
+	elif weakpoint.lower() == "wis":
+		char.wis = 8
+	elif weakpoint.lower() == "char":
+		char.char = 8
+	print ""
+	print "You start with the following stats: \nStr: %s Con: %s Dex: %s Int: %s Wis: %s Char: %s" %(char.str, char.con, char.dex, char.int, char.wis, char.char)
+	print ""	
+	print "You have %s points left" %(points_left)
+	print "The cost of improving scales up as follows: \nScore   Cost \n9       -(1) \n10      0(2) \n11      1 \n12      2 \n13      3 \n14      5 \n15      7 \n16      9 \n17      12 \n18      16"
+	choice_str = int(raw_input("What should your strength be? "))
+	if weakpoint.lower() == "str":
+		while choice_str > 18 or choice_str < 8:
+			print "Try a different score"
+			choice_str = int(raw_input("What should your strength be? "))
+		while attribute_cost_weakness[choice_str] > points_left:
+			print "Not enough points left."
+			choice_str = int(raw_input("What should your strength be? "))
+		points_left = points_left - attribute_cost_weakness[choice_str]
 	else:
-		for i in range(throws + abs(advantage)):
-			result.append(randint(1, sides))
-		if advantage > 0:
-			for i in range (advantage):
-				result.remove(min(result))
-		elif advantage < 0:
-			for i in range (abs(advantage)):
-				result.remove(max(result))
-	return result
-
-
+		while choice_str > 18 or choice_str < 10:
+			print "Try a different score"
+			choice_str = int(raw_input("What should your strength be? "))
+		while attribute_cost[choice_str] > points_left:
+			print "Not enough points left."
+			choice_str = int(raw_input("What should your strength be? "))
+		points_left = points_left - attribute_cost[choice_str]
 	
-def new_char_input():
-	global char
-	char = Characters(
-		0,
-		raw_input("Are you male or female? "),
-		raw_input("What is your name? "),
-		raw_input("You are the representative of which race? "),
-		int(raw_input("How old are you? ")),
-		int(raw_input("How tall are you? ")),
-		int(raw_input("How fat are you? ")),
-		raw_input("What is your class? "),
-		raw_input("What is your alignment? "),
-		raw_input("Who is your deity? ")
-	)
-
+	print ""
+	print "You have %s points left" %(points_left)
 	
-"""
-
-def new_char_input():
-	## These choices are cosmetic and will not change anything later on
-	char["Gender"] = raw_input("Are you male or female? ")
-	char["Name"] = raw_input("What is your name? ")
-	char["Race"] = raw_input("You are the representative of which race? ")
-	while char["Race"].lower() not in races:
-		print "Not a valid race."
-		char["Race"] = raw_input("You are the representative of which race? ")
-	char["Age"] = raw_input("How old are you? ")
-	char["Height"] = raw_input("How tall are you? ")
-	char["Weight"] = raw_input("How fat are you? ")
-
-	## These choices should be limited and have impact on other things
-	char["Class"] = raw_input("Please enter your class. ")
-	while char["Class"].lower() not in classes:
-		print "Not a valid class."
-		char["Class"] = raw_input("Please enter your class. ")
-	char["Alignment"] = raw_input("Please enter your alignment. ")
-	char["Deity"] = raw_input("Who is your deity? ")
-
-
-"""	
+	choice_con = int(raw_input("What should your constitution be? "))
+	if weakpoint.lower() == "con":
+		print "This is your weakness, you start at 8 points."
+		while choice_con > 18 or choice_con <8:
+			print "Try a different score"
+			choice_con = int(raw_input("What should your constitution be? "))
+		while attribute_cost_weakness[choice_con] > points_left:
+			print "Not enough points left."
+			choice_con = int(raw_input("What should your constitution be? "))
+		points_left = points_left - attribute_cost_weakness[choice_con]
+	else:
+		while choice_con > 18 or choice_con <10:
+			print "Try a different score"
+			choice_con = int(raw_input("What should your constitution be? "))
+		while attribute_cost[choice_con] > points_left:
+			print "Not enough points left."
+			choice_con = int(raw_input("What should your constitution be? "))
+		points_left = points_left - attribute_cost[choice_con]
+	
+	print ""
+	print "You have %s points left" %(points_left)
+	
+	choice_dex = int(raw_input("What should your dexterity be? "))
+	if weakpoint.lower() == "dex":
+		print "This is your weakness, you start at 8 points."
+		while choice_dex > 18 or choice_dex <8:
+			print "Try a different score"
+			choice_dex = int(raw_input("What should your dexterity be? "))
+		while attribute_cost_weakness[choice_dex] > points_left:
+			print "Not enough points left."
+			choice_dex = int(raw_input("What should your dexterity be? "))
+		points_left = points_left - attribute_cost_weakness[choice_dex]
+	else:
+		while choice_dex > 18 or choice_dex <10:
+			print "Try a different score"
+			choice_dex = int(raw_input("What should your dexterity be? "))
+		while attribute_cost[choice_dex] > points_left:
+			print "Not enough points left."
+			choice_dex = int(raw_input("What should your dexterity be? "))
+		points_left = points_left - attribute_cost[choice_dex]
+	
+	print ""
+	print "You have %s points left" %(points_left)
+	
+	choice_int = int(raw_input("What should your intelligence be? "))
+	if weakpoint.lower() == "int":
+		print "This is your weakness, you start at 8 points."
+		while choice_int > 18 or choice_int <8:
+			print "Try a different score"
+			choice_int = int(raw_input("What should your intelligence be? "))
+		while attribute_cost_weakness[choice_int] > points_left:
+			print "Not enough points left."
+			choice_int = int(raw_input("What should your intelligence be? "))
+		points_left = points_left - attribute_cost_weakness[choice_int]
+	else:
+		while choice_int > 18 or choice_int <10:
+			print "Try a different score"
+			choice_int = int(raw_input("What should your intelligence be? "))
+		while attribute_cost[choice_int] > points_left:
+			print "Not enough points left."
+			choice_int = int(raw_input("What should your intelligence be? "))
+		points_left = points_left - attribute_cost[choice_int]
+	
+	print ""
+	print "You have %s points left" %(points_left)
+	
+	choice_wis = int(raw_input("What should your wisdom be? "))
+	if weakpoint.lower() == "wis":
+		print "This is your weakness, you start at 8 points."
+		while choice_wis > 18 or choice_wis <8:
+			print "Try a different score"
+			choice_wis = int(raw_input("What should your wisdom be? "))
+		while attribute_cost_weakness[choice_wis] > points_left:
+			print "Not enough points left."
+			choice_wis = int(raw_input("What should your wisdom be? "))
+		points_left = points_left - attribute_cost_weakness[choice_wis]
+	else:
+		while choice_wis > 18 or choice_wis <10:
+			print "Try a different score"
+			choice_wis = int(raw_input("What should your wisdom be? "))
+		while attribute_cost[choice_wis] > points_left:
+			print "Not enough points left."
+			choice_wis = int(raw_input("What should your wisdom be? "))
+		points_left = points_left - attribute_cost[choice_wis]
+	
+	print ""
+	print "You have %s points left" %(points_left)
+	
+	choice_char = int(raw_input("What should your charisma be? "))
+	if weakpoint.lower() == "char":
+		print "This is your weakness, you start at 8 points."
+		while choice_char > 18 or choice_char <8:
+			print "Try a different score"
+			choice_char = int(raw_input("What should your charisma be? "))
+		while attribute_cost_weakness[choice_char] > points_left:
+			print "Not enough points left."
+			choice_char = int(raw_input("What should your charisma be? "))
+		points_left = points_left - attribute_cost_weakness[choice_char]
+	else:
+		while choice_char > 18 or choice_char <10:
+			print "Try a different score"
+			choice_char = int(raw_input("What should your charisma be? "))
+		while attribute_cost[choice_char] > points_left:
+			print "Not enough points left."
+			choice_char = int(raw_input("What should your charisma be? "))
+		points_left = points_left - attribute_cost[choice_char]
+	
+	char.str = choice_str
+	char.con = choice_con
+	char.dex = choice_dex
+	char.int = choice_int
+	char.wis = choice_wis
+	char.char = choice_char
+	
+	print ""
+	print "You now have the following stats: \nStr: %s Con: %s Dex: %s Int: %s Wis: %s Char: %s" %(char.str, char.con, char.dex, char.int, char.wis, char.char)
+	print ""
+	user_confirmation = raw_input("Are you happy with your stats? (y/n) ")
+	if user_confirmation.lower() == "n" or user_confirmation.lower() == "no":
+		atr_by_choice()
+	
+atr_by_choice()
 	
 def new_char_atr():
 	user_creation_choice = raw_input("random attributes? (y/n) ")
@@ -224,9 +266,6 @@ def new_char_atr():
 		char.wis = sum(d(6, 3, 1))
 		char.char = sum(d(6, 3, 1))
 		print "You rolled the following stats: \nStr: %s Con: %s Dex: %s Int: %s Wis: %s Char: %s" %(char.str, char.con, char.dex, char.int, char.wis, char.char)
-		char.race.ability_scores()
-		print "You rolled the following stats: \nStr: %s Con: %s Dex: %s Int: %s Wis: %s Char: %s" %(char.str, char.con, char.dex, char.int, char.wis, char.char)
-		
 	elif user_creation_choice.lower() == "n" or user_creation_choice.lower() == "no":
 		attributes = ["str", "con", "dex", "int", "wis", "char"]
 		attribute_cost = {10:0, 11:1, 12:2, 13:3, 14:5, 15:7, 16:9, 17:12, 18:16}
@@ -398,81 +437,3 @@ def new_char_atr():
 	else:
 		print "Sorry, I don't understand."
 		new_char_atr()
-
-		
-def interface():
-	choice = raw_input("\n|---------------|\n|      NEW      |\n|---------------|\n|     DICE      |\n|---------------|\n| SEE CHARACTER |\n|---------------|\n|     SAVE      |\n|---------------|\n|     LOAD      |\n|---------------|\n|     EXIT      |\n|---------------|\n \n")
-	if choice == "new":
-		new_char_input()
-		new_char_atr()
-	elif choice == "dice":
-		side = int(raw_input("How many sides does the dice have? "))
-		throws = int(raw_input("How many throws do you want to make? "))
-		advantage = int(raw_input("What is your advantage? "))
-		print d(side, throws, advantage)
-		print ""
-		raw_input("")
-	elif choice == "see character":
-		if "char" in globals():
-			print "\nD&D    Character Sheet    D&D\n-----------------------------\nNAME:       %s\nEXP:        %s\nLEVEL:      %s\nGENDER:     %s\nRACE:       %s\nAGE:        %s\nHEIGHT:     %s\nWEIGHT:     %s\nCLASS:      %s\nALIGNMENT:  %s\nDEITY:      %s\n-----------------------------\nSTR:        %s\nCON:        %s\nDEX:        %s\nINT:        %s\nWIS:        %s\nCHAR:       %s\n \n" %(char.name, char.exp, char.level(), char.gender, char.race.name, char.age, char.height, char.weight, char.char_class, char.alignment, char.deity, char.str, char.con, char.dex, char.int, char.wis, char.char)
-			raw_input("")
-		else:
-			print "No character available"
-			raw_input("")
-	elif choice == "save":
-		save()
-	elif choice == "load":
-		load()
-	elif choice == "exit":
-		exit()
-		
-while gamestate == "continue":
-	interface()
-		
-		
-
-
-"""
-
-Future structure for inventory
-
-class Inventory(object):
-    items_in_inventory = {}
-    def __init__(self, char_name):
-        self.char_name = char_name
-
-    def add_item(self, item, amount):
-		self.items_in_inventory[item] += amount
-		print product + " added."
-
-    def remove_item(self, item, amount):
-        if item in self.items_in_inventory and self.items_in_inventory[item] > 0:
-            self.items_in_inventory[item] += -amount
-            print "%s %s removed." %(amount, item)
-        else:
-            print item + " is not in the inventory."
-
-"""
-		
-
-##	Strength represents physical power.
-##	Constitution represents health and stamina.
-##	Dexterity represents agility and reflexes.
-##	Intelligence represents reasoning.
-##	Wisdom represents common sense.
-##	Charisma represents personality.
-
-
-"""
-
-Future plans:
-
-- Menu
-- Online capability
-- Multiple characters
-- UI
-
-
-"""
-
-## By Versace_Python
